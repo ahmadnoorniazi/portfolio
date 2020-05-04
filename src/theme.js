14471027f7f28fd9c5860d6cee6a61c0133a7319
ahmadnoor <ahmad.noor@mobilelive.ca>
2020-05-05 00:11:40 +0500

    add global theme

---
+++ src/theme.js
@@ -0,0 +1,28 @@
+const theme = {
+  fontSizes: [
+    12, 14, 16, 24, 32, 48, 64,
+  ],
+  colors: {
+    primary: '#000',
+    secondary: '#08d665',
+    textColor: '#c8c6c6',
+    white: '#FFFFFF',
+  },
+  buttons: {
+    primary: {
+      color: 'white',
+      bg: 'secondary',
+    },
+    outline: {
+      color: 'secondary',
+      bg: 'primary',
+      border: '1px solid secondary'
+    },
+    disabled: {
+      bg: '#E5E5E5',
+      color: 'black',
+    },
+  },
+};
+
+export default theme;
