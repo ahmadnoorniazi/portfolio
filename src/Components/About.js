3535b026725cb04ad908d1cb9235f99cf3a8ef76
ahmadnoor <ahmad.noor@mobilelive.ca>
2020-05-05 00:11:57 +0500

    Refector about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,56 +1,51 @@
-/* eslint-disable */
-import React, { Component } from 'react';
+import React from 'react';
+import {
+  Box, Flex as Wrapper, Image
+} from 'rebass';
+import styled from 'styled-components';
+import { layout, compose, position } from 'styled-system';
+import Heading from './Heading';
 
-class About extends Component {
-  render() {
-
-    if(this.props.data){
-      var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
-      var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
-    };
-
-    return (
-      <section id="about">
-      <div className="row">
-         <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
-         </div>
-         <div className="nine columns main-col">
-            <h2>About Me</h2>
-
-            <p>{bio}</p>
-            <div className="row">
-               <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-						   <span>{name}</span><br />
-						   <span>{street}<br />
-						         {city} {state}, {zip}
-                   </span><br />
-						   <span>{phone}</span><br />
-                     <span>{email}</span>
-					   </p>
-               </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
-               </div>
-            </div>
-         </div>
-      </div>
-
-   </section>
-    );
-  }
-}
+const Flex = styled(Wrapper)`
+${compose(layout, position)}`;
+const About = () => (
+  <Flex
+    id="about"
+    bg="primary"
+    width={1}
+    height="700px"
+    justifyContent="center"
+    sx={{
+      position: 'relative'
+    }}
+  >
+    <Flex
+      position="absolute"
+      left={[0, '5%', '20%']}
+      top={['10%', '10%', '25%']}
+      bottom={['10%', '10%', '25%']}
+      flexDirection={['column', 'column', 'row']}
+    >
+      <Box width={[1, 1, 1 / 3]}>
+        <Image
+          sx={{
+            borderRadius: '03%'
+          }}
+          width={['350px']}
+          height={['350px']}
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSQvq-NTsapFOvnWCVeU-guFDAQWyI5yCRyEzFnwYApa5zByx-R&usqp=CAU"
+        />
+      </Box>
+      <Box width={['90%', 1 / 2]} mr={['10px', '10px', 0]} ml={['10px', '10px', '40px']} sx={{ textAlign: 'center', margin: 'auto' }}>
+        <Heading color="white" as="h1">
+          FAILURE IS THE CONDIMENT THAT GIVES
+          <Heading ml={2} as="span" color="secondary">
+            SUCCESS
+          </Heading>
+        </Heading>
+      </Box>
+    </Flex>
+  </Flex>
+);
 
 export default About;
