5f9f5c671bba573ac7c1419bc82948002cbd7695
ahmadnoor <ahmad.noor@mobilelive.ca>
2020-05-06 00:50:41 +0500

    Add Border Text Component

---
+++ src/Components/BorderText/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text, Box as Wrapper } from 'rebass';
+import styled from 'styled-components';
+import {
+  compose, border
+} from 'styled-system';
+import theme from '../../theme';
+
+const Box = styled(Wrapper)`
+${compose(border)}`;
+
+const BorderText = ({ children, textAlign }) => (
+  <Box borderLeft={`3px solid ${theme.colors.secondary}`}>
+    <Text color="white" textAlign={textAlign} ml="20px" as="p" fontSize="18px" sx={{ 'text-transform': 'capitalize' }} fontWeight={400}>
+      {children}
+    </Text>
+  </Box>
+);
+
+export default BorderText;
