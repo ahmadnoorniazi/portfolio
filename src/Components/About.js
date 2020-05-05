57fde3bf76c0841e435c7aa6bbbecd81597878b0
ahmadnoor <ahmad.noor@mobilelive.ca>
2020-05-06 00:49:43 +0500

    Add new headings to about section

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import {
-  Box, Flex as Wrapper, Image
+  Box as Main, Flex as Wrapper, Image, Text
 } from 'rebass';
 import styled from 'styled-components';
-import { layout, compose, position } from 'styled-system';
-import Heading from './Heading';
+import {
+  layout, compose, position, typography
+} from 'styled-system';
+import Button from './Button';
+import SpanText from './SpanText';
 
 const Flex = styled(Wrapper)`
 ${compose(layout, position)}`;
+const Box = styled(Main)`
+${compose(typography)}`;
+
 const About = () => (
   <Flex
     id="about"
     bg="primary"
+    mb="30px"
     width={1}
     height="700px"
     justifyContent="center"
@@ -36,13 +43,23 @@ const About = () => (
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSQvq-NTsapFOvnWCVeU-guFDAQWyI5yCRyEzFnwYApa5zByx-R&usqp=CAU"
         />
       </Box>
-      <Box width={['90%', 1 / 2]} mr={['10px', '10px', 0]} ml={['10px', '10px', '40px']} sx={{ textAlign: 'center', margin: 'auto' }}>
-        <Heading color="white" as="h1">
-          FAILURE IS THE CONDIMENT THAT GIVES
-          <Heading ml={2} as="span" color="secondary">
-            SUCCESS
-          </Heading>
-        </Heading>
+      <Box
+        textAlign={['center', 'center', 'initial']}
+        width={['90%', 1 / 2]}
+        mr={['10px', '10px', 0]}
+        ml={['15px', '15px', '40px']}
+        mt={10}
+        sx={{ margin: 'auto' }}
+      >
+        <SpanText firstText="FAILURE IS THE CONDIMENT THAT GIVES" secondText="SUCCESS" />
+        <Text as="p" fontWeight={400} sx={{ textAlign: 'justify' }} color="white" my={[20, 20, 25]}>
+          Spend more time focusing on the important aspects of your business.
+          Turn to McCartney HR LLC in Brooklyn, NY for HR solutions. As an advanced
+          virtual HR company, we are offering online HR systems that can be customized depending on your business needs.
+        </Text>
+        <Button px={30}>
+          Download CV
+        </Button>
       </Box>
     </Flex>
   </Flex>
