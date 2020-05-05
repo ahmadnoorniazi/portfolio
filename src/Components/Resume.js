4da9d3ef476624d42e2a7d5d903b84cc0f186102
ahmadnoor <ahmad.noor@mobilelive.ca>
2020-05-06 00:50:05 +0500

    update resume section

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -6,15 +6,29 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { Flex, Box, Text } from 'rebass';
 import Cards from './Card';
+import SpanText from './SpanText';
+import BorderText from './BorderText';
+import Paragraph from './Paragraph';
 
 function WorkDetail({data, heading, span}) {
   return (
-    <Box bg="primary" id="works">
-      <Box sx={{textAlign: "center"}} mb={30}>
-        <Text color="#FFFFFF" as="h2">Where i've Worked</Text>
-      </Box>
-    <Flex justifyContent="center" p={['20', '10', 0]} m={[0,0,50]} flexWrap='wrap' flexDirection={['column', 'column', "row"]}>
-      {data && data.work && data.work.map(item => (
+    <Box bg="primary" id="works" pt={50} px={[10, 10, 50]}>
+    <Flex justifyContent="space-between" p={['10', '10', 0]} m={[0,0,50]} flexWrap='wrap' flexDirection={['column', 'column', "row"]}>
+      <Flex px={15} sx={{textAlign: "center"}} mb={30} flexDirection={['column', 'column', "row"]}>
+        <Box width={[1,1, 1 / 2]} my={20}>
+          <Paragraph fontFamily="sans-serif" sx={{ textTransform: 'uppercase' }} fontSize="16px" lineHeight="16px" color="secondary" fontWeight={800} textAlign={['center', 'center', 'initial']}>
+            My Experience
+          </Paragraph>
+          <SpanText textAlign={["center", 'center', 'initial']} firstText="Experience and" secondText="skill" />
+        </Box>
+          <Box width={[1, 1, 1 / 2]} my={20} px="15px">
+            <BorderText  textAlign={["justify", 'justify', 'initial']}>
+            Lorem Ipsum Dolor Sit Amet, Consectetur Adipisicing Elit. Sint Ratione Reprehenderit,
+            Error Qui Enim Sit Ex Provident
+            </BorderText>
+          </Box>
+      </Flex> 
+    {data && data.work && data.work.map(item => (
       <Box width={[1,1,1 / 4]} px={15} my={20}>
       <Cards 
         imagePath={`/images/${item.imageKey}`}
