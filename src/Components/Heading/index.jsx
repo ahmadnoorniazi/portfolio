48f71f9881e0fe9f89587c86cd7d9686d0186946
ahmadnoor <ahmad.noor@mobilelive.ca>
2020-05-18 05:27:22 +0500

    update Heading Component

--- src/Components/Heading/index.jsx
+++ src/Components/Heading/index.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 import { Text } from 'rebass';
 
 const Heading = ({ children, ...rest }) => (
-  <Text {...rest} lineHeight={['25px', '25px', '55px']} fontSize={['20px', '20px', '44px']} letterSpacing="-2">
+  <Text
+    letterSpacing="-2"
+    as="h1"
+    {...rest}
+  >
     {children}
   </Text>
 );
