0e1372750a3da30b8dd62b44fe599b47b864f6ad
ahmadnoor <ahmad.noor@mobilelive.ca>
2020-05-18 05:25:52 +0500

    add colors

---
+++ src/utils/colors.js
@@ -0,0 +1,7 @@
+export const primary = '#000';
+export const secondary = '#08d665';
+export const hover = '#09c75f';
+export const textColor = '#c8c6c6';
+export const white = '#FFFFFF';
+export const background = 'rgb(25, 26, 27)';
+export const grayBorder = '#131617';
