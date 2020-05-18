e9949ecc5a5659e2880b5bcbd94e63eca9256b2d
ahmadnoor <ahmad.noor@mobilelive.ca>
2020-05-18 05:22:10 +0500

    update theme obj

--- src/theme.js
+++ src/theme.js
@@ -1,24 +1,66 @@
+import {
+  primary,
+  secondary,
+  hover,
+  textColor,
+  white,
+  background,
+  grayBorder,
+} from './utils/colors';
+
+const secondaryVariant = {
+  color: secondary,
+  bg: primary,
+  border: `1px solid ${secondary}`
+};
+
+const primaryVariant = {
+  color: white,
+  bg: secondary,
+};
+
 const theme = {
+  breakpoints: ['40em', '52em', '64em'],
   fontSizes: [
     12, 14, 16, 24, 32, 48, 64,
   ],
+  space: [
+    0, 4, 8, 12, 14, 16, 32, 64, 128, 256
+  ],
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.25,
+  },
   colors: {
-    primary: '#000',
-    secondary: '#08d665',
-    hover: '#09c75f',
-    textColor: '#c8c6c6',
-    white: '#FFFFFF',
-    background: 'rgb(25, 26, 27)'
+    primary,
+    secondary,
+    hover,
+    textColor,
+    white,
+    background,
+    grayBorder,
+  },
+  variants: {
   },
   buttons: {
-    primary: {
-      color: 'white',
-      bg: 'secondary',
+    secondary: secondaryVariant,
+    light: {
+      ...primaryVariant,
+      ':hover': {
+        ...secondaryVariant,
+        bg: white
+      }
     },
-    outline: {
-      color: 'secondary',
-      bg: 'primary',
-      border: '1px solid secondary'
+    primary: {
+      ...primaryVariant,
+      ':hover': {
+        ...secondaryVariant
+      }
     },
     disabled: {
       bg: '#E5E5E5',
